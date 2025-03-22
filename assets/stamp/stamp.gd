d320846e7b7ea954a32ecd67be878337e475221a
Mateo Delacote <Mateo.delacote@gmail.com>
2025-03-22 12:50:37 -0400

    better stamp and buro

--- assets/stamp/stamp.gd
+++ assets/stamp/stamp.gd
@@ -6,13 +6,13 @@ var refused_mark = preload ("res://assets/stamp/refused_mark.tscn")
 var is_charged = true
 
 
-func stamp(feuille):
+func stamp(feuille:Node2D):
 	if is_charged:
 		var mark = null
 		if is_accepted_stamp:
 			mark = accepted_mark.instantiate()
 		else:
 			mark = refused_mark.instantiate()
-		feuille.add_child(mark)
+		feuille.get_child(0).add_child(mark)
 		mark.global_position = global_position
 		feuille.is_accepted = is_accepted_stamp

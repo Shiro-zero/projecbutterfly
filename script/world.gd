ad59865c42026e5bc21dc921fdc95441aeb33386
Antoine Auger <Ardyn142@gmail.com>
2025-03-22 18:56:21 -0400

    add stats

--- script/world.gd
+++ script/world.gd
@@ -1,11 +1,12 @@
 extends Node2D
 var preFeuille = preload("res://assets/feuille papier/feuille_papier.tscn")
 func _ready():
-	spawn_loi(5)
+	spawn_loi(3)
 	
 func spawn_loi(n:int):
 	for i in range(n):
 		var temp = preFeuille.instantiate()
+		temp.set_variable(i)
 		$DraggableController.add_child(temp)
 		temp.position=Vector2(800,400)
 		temp.rotation=randf_range(-.05, .05)

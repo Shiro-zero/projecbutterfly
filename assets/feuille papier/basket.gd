37a584b8c70923ff8d8213b2ffbe37e00c6b379f
Jean-LoupIsReal <jean.loupca@hotmail.fr>
2025-03-22 22:26:12 -0400

    plus dialogue, knock_back feuille, stat change

--- assets/feuille papier/basket.gd
+++ assets/feuille papier/basket.gd
@@ -10,6 +10,4 @@ func drop_feuille_in_basket_animation(feuille):
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(feuille, "global_position", global_position, duration)
 	tween.tween_property(self, "rotation_degrees", 0.0, 1.0)
-
-
 	feuille.applyStat()
